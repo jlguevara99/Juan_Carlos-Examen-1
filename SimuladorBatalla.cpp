c69bcf1e5f8d32a6028058efd2fccbfcbc62d397
jlguevara99 <jlguevara99@hotmail.com>
2017-08-18 17:46:57 -0600

    Avanzando

---
+++ SimuladorBatalla.cpp
@@ -0,0 +1,3 @@
+#include "SimuladorBatalla.h"
+
+
---
+++ SimuladorBatalla.h
@@ -0,0 +1,32 @@
+#ifndef SIMULADORBATALLA_H
+#define SIMULADORBATALLA_H
+#include<string>
+#include "FamiliaStark.h"
+#include "FamiliaLannister.h"
+#include "FamiliaTargaryen.h"
+#include "Dothraki.h"
+#include "Dragon.h"
+#include "GuardiaReal.h"
+using namespace std;
+
+
+class SimuladorBatalla{
+	public: 
+			
+
+
+
+	private:
+		void Batalla();
+	
+};
+
+
+
+
+
+
+
+
+
+#endif
